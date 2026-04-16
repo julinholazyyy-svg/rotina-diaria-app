9d33bd608be788826619592cc7bce5f83616b3e0
julinholazyyy-svg <julinho.lazyyy@gmail.com>
2026-04-16 16:08:38 +0100

    Refactor comprar and add filtrar function

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function comprar(produto) {
-  alert("🍫 " + produto + " adicionado ao carrinho!");
+let cart = 0;
+
+function comprar() {
+  cart++;
+  document.getElementById("cartCount").innerText = cart;
 }
 
-function scrollProdutos() {
-  document.getElementById("produtos").scrollIntoView({
-    behavior: "smooth"
+function filtrar() {
+  let input = document.getElementById("search").value.toLowerCase();
+  let cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    let nome = card.getAttribute("data-name");
+    if (nome.includes(input)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
   });
 }
