5919557b6e0f7bf12552ab28bd4e2130b70bc7f3
julinholazyyy-svg <julinho.lazyyy@gmail.com>
2026-04-16 16:51:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,8 @@
-let cart = [];
+let cart = 0;
 
 function comprar() {
-  // pega info do card clicado
-  let card = event.target.parentElement;
-
-  let nome = card.querySelector("h3").innerText;
-  let precoText = card.querySelector("span").innerText;
-  let preco = parseFloat(precoText.replace("€", "").replace(",", "."));
-
-  cart.push({ nome, preco });
-
-  atualizarCarrinho();
-}
-
-function atualizarCarrinho() {
-  let cartCount = document.getElementById("cartCount");
-  cartCount.innerText = cart.length;
-
-  // cria/atualiza painel do carrinho
-  let painel = document.getElementById("cartPanel");
-
-  if (!painel) {
-    painel = document.createElement("div");
-    painel.id = "cartPanel";
-    document.body.appendChild(painel);
-  }
-
-  let html = "<h3>🛒 Carrinho</h3>";
-
-  let total = 0;
-
-  cart.forEach((item, index) => {
-    total += item.preco;
-
-    html += `
-      <div class="cart-item">
-        <p>${item.nome}</p>
-        <span>${item.preco.toFixed(2)}€</span>
-      </div>
-    `;
-  });
-
-  html += `<hr><strong>Total: ${total.toFixed(2)}€</strong>`;
-
-  painel.innerHTML = html;
+  cart++;
+  document.getElementById("cartCount").innerText = cart;
 }
 
 function filtrar() {
